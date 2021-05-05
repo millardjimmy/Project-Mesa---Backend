7b6846e122cd0258feaba26759cd5696e20955db
millardjimmy <millardjimmy@gmail.com>
2021-05-05 13:51:59 -0600

    now able to create transactions with validations

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -13,11 +13,16 @@ class Api::V1::TransactionsController < ApplicationController
     end
 
     def create
-       
+       @transaction = @account.transactions.build(transaction_params)
+       if @account.update_balance(@transaction) != 'Balance too low.'
+            @transaction.save
+            render json: @transaction
+       else
+            render json: {error: 'Balance too low.'}
+       end
     end
 
     def destory
-      
     end
 
     private
