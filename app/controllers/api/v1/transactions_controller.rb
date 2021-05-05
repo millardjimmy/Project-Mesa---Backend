4182f504412ca51cfc64d552f10ff8654b510499
millardjimmy <millardjimmy@gmail.com>
2021-05-05 13:33:01 -0600

    set up before action to localize Account methods

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,5 +1,32 @@
 class Api::V1::TransactionsController < ApplicationController
 
+    before_action :set_account
+
+    def index
+        
+    end
+
+    def create
+       
+    end
+
+    def show
+       
+    end
+
+    def destory
+      
+    end
+
+    private
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end
+
+    def transaction_params
+        params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
+    end
 
 
 end
